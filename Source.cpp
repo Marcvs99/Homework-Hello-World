4249156b7771c0c73069c532fdcea45613898683
Marcus King <marcvs99@gmail.com>
2022-03-14 22:02:56 -0400

    first commi

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
 
-	cout << "Hello World!"; /*prints literal text to console*/
+	cout << "Hello World!!"; /*prints literal text to console*/
 
 
 	system("pause");
