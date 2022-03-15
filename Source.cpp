acd8887c2babaf873238796b5b694615630e4524
Work <Work@DESKTOP-FGML920>
2022-03-14 21:45:59 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+/*Name: Marcus King
+* Date: 8/24/21
+* Program Name:
+Program Description: Print "Hello World! to console*/
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World!"; /*prints literal text to console*/
+
+
+	system("pause");
+	return 0;
+}
